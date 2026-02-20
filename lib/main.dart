fa685c805f865428df1460d950fd4fdb8305e02b
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-20 11:26:30 +0700

    menambahkan library provider di pubspec.yaml dan menggunakan ChangeNotifierProvider untuk bisa memanggil model CounterModel yang menggunakan CounterNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import './views/home_page.dart';
+import './models/counter_model.dart';
+import 'package:provider/provider.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
